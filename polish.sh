be5a037131cc191d2b523b8913c3e749e16a2a94
Ibrahim <mohammed.ibrahim@practo.com>
2017-03-22 12:55:59 +0000

    updating polist

--- polish.sh
+++ polish.sh
@@ -2,3 +2,5 @@
 find . -name "*.java" -exec sed -i 's/\t/    /g' {} +
 find . -name "*.java" -exec sed -i 's/[ \t]*$//' {} +
 
+find . -name "*.ju" -exec sed -i 's/\t/    /g' {} +
+find . -name "*.ju" -exec sed -i 's/[ \t]*$//' {} +
