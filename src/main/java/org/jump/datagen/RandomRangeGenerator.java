3bac3b4d49385899a08a004fd8d3a5a266169e92
Ibrahim <mohammed.ibrahim@practo.com>
2017-04-10 04:27:34 +0000

    Adding double support for method random_between

--- src/main/java/org/jump/datagen/RandomRangeGenerator.java
+++ src/main/java/org/jump/datagen/RandomRangeGenerator.java
@@ -2,16 +2,19 @@ package org.jump.datagen;
 
 import java.util.Random;
 
+import org.jump.entity.DataType;
 import org.jump.parser.FieldConfig;
 import org.jump.util.Utility;
 
 public class RandomRangeGenerator implements IField {
 
-    private Integer min;
+    private Double min;
 
-    private Integer max;
+    private Double max;
 
     private Random randomizer = new Random();
+
+    private DataType dataType;
 
     public RandomRangeGenerator(FieldConfig fieldConfig) {
 
@@ -19,27 +22,54 @@ public class RandomRangeGenerator implements IField {
             throw new RuntimeException("random_between takes 2 arguments.");
         }
 
-        if (!Utility.isNumeric(fieldConfig.getParams().get(0))
-            || !Utility.isNumeric(fieldConfig.getParams().get(1))) {
+        if (!Utility.isNumeric(fieldConfig.getParams().get(0)) || !Utility.isNumeric(fieldConfig.getParams().get(1))) {
 
-            throw new RuntimeException("Argument for function random_between have to be integers");
+            throw new RuntimeException("Argument for function random_between have to be either integer or double");
         }
 
         Double p1 = Double.parseDouble(fieldConfig.getParams().get(0));
         Double p2 = Double.parseDouble(fieldConfig.getParams().get(1));
 
-        this.min = new Double(Math.min(p1.doubleValue(), p2.doubleValue())).intValue();
-        this.max = new Double(Math.max(p1.doubleValue(), p2.doubleValue())).intValue();
+        this.min = new Double(Math.min(p1.doubleValue(), p2.doubleValue()));
+        this.max = new Double(Math.max(p1.doubleValue(), p2.doubleValue()));
 
         if (this.min.equals(this.max)) {
             throw new RuntimeException("min and max value for random_between cannot be same.");
+        }
+
+        if (fieldConfig.getParams().get(0).contains(".") || fieldConfig.getParams().get(1).contains(".")) {
+            this.dataType = DataType.DOUBLE;
+        } else {
+            this.dataType = DataType.INTEGER;
         }
     }
 
     @Override
     public String getNext() {
-        Integer result = randomizer.nextInt(this.max - this.min) + this.min;
-        return Utility.wrapAndEscape(String.valueOf(result.intValue()));
+        String result = null;
+        switch (this.dataType) {
+            case INTEGER:
+                Integer intValue = randomizer.nextInt(this.max.intValue() - this.min.intValue()) + this.min.intValue();
+                result = String.valueOf(intValue);
+                break;
+
+            case DOUBLE:
+                Double doubleValue = randomInRange(this.min, this.max);
+                result = String.valueOf(doubleValue);
+                break;
+
+            default:
+                String message = "Unsupported dataType: " + this.dataType.toString();
+                throw new RuntimeException(message);
+        }
+
+        return Utility.wrapAndEscape(result);
     }
 
+    private double randomInRange(double min, double max) {
+        double range = max - min;
+        double scaled = randomizer.nextDouble() * range;
+        double shifted = scaled + min;
+        return shifted; // == (rand.nextDouble() * (max-min)) + min;
+    }
 }
